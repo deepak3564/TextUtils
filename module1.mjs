8ca415349688b36ed2762d64ae5eb4734fb2e31b
deepak3564 <112762309+deepak3564@users.noreply.github.com>
2022-12-21 16:11:11 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,9 @@
+const a="Deepak";
+const b="Vaibhav";
+const c="Vishal";
+const d="Praveen";
+
+export default d;
+export {a};
+export {b};
+export {c};
---
+++ module2.mjs
@@ -0,0 +1,2 @@
+import {c} from './module1.mjs'
+console.log(c);
